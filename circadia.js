6ec6e44f96baf49da6da5dff5e9d6572258a8b76
Rohin Tangirala <rohinkt@gmail.com>
2019-07-21 16:49:50 -0700

    standard style

--- circadia.js
+++ circadia.js
@@ -1,12 +1,12 @@
 class Circadia {
-  constructor() {
+  constructor () {
     this.styles = {
       background: 'rgb(0, 0, 0)'
-    };
-    this.originalDom = document.body.cloneNode(true);
-    
+    }
+    this.originalDom = document.body.cloneNode(true)
+
     if (localStorage.getItem('activated' === null)) {
-      localStorage.setItem('activated', 'false');
+      localStorage.setItem('activated', 'false')
     }
 
     if (localStorage.getItem('activated') === 'true') {
@@ -16,80 +16,79 @@ class Circadia {
     }
   }
 
-  activate() {
-    this.attachStyles(this.styles);
-    this.adjustColors();
-    localStorage.setItem('activated', 'true');
+  activate () {
+    this.attachStyles(this.styles)
+    this.adjustColors()
+    localStorage.setItem('activated', 'true')
   }
 
-  deactivate() {
-    document.body = this.originalDom;
-    localStorage.setItem('activated', 'false');
+  deactivate () {
+    document.body = this.originalDom
+    localStorage.setItem('activated', 'false')
   }
 
-  adjustColors() {
+  adjustColors () {
     document.body.querySelectorAll('*').forEach((node) => {
-      var nodeComputedStyle = window.getComputedStyle(node);
-      var parentNodeComputedStyle = window.getComputedStyle(node.parentNode);
+      var nodeComputedStyle = window.getComputedStyle(node)
+      var parentNodeComputedStyle = window.getComputedStyle(node.parentNode)
 
       if (nodeComputedStyle.backgroundColor === 'rgba(0, 0, 0, 0)' && parentNodeComputedStyle.backgroundColor === 'rgba(0, 0, 0, 0)') {
-        var originalColor = window.getComputedStyle(node).color;
-        var parsedColor = this.parseRgb(originalColor);
-        var luminance = this.relativeLuminance(parsedColor);
-        var contrastRatio = this.contrast(luminance);
+        var originalColor = window.getComputedStyle(node).color
+        var parsedColor = this.parseRgb(originalColor)
+        var luminance = this.relativeLuminance(parsedColor)
+        var contrastRatio = this.contrast(luminance)
 
         if (contrastRatio < 4.5) {
-          var rgbBrightened = this.brighten(parsedColor);
-          var formattedColor = `rgb(${rgbBrightened[0]}, ${rgbBrightened[1]}, ${rgbBrightened[2]})`;
-          node.style.color = formattedColor;
+          var rgbBrightened = this.brighten(parsedColor)
+          var formattedColor = `rgb(${rgbBrightened[0]}, ${rgbBrightened[1]}, ${rgbBrightened[2]})`
+          node.style.color = formattedColor
         }
       }
-    });
+    })
   }
 
-  attachStyles(styles) {
+  attachStyles (styles) {
     Object.keys(styles).forEach((key, i) => {
-      document.body.style[key] = styles[key];
-    });
-  } 
+      document.body.style[key] = styles[key]
+    })
+  }
 
-  parseRgb(color) {
-    var parsed = color.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i);
+  parseRgb (color) {
+    var parsed = color.match(/^rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i)
 
     if (parsed) {
-      return [parsed[1], parsed[2], parsed[3]];
+      return [parsed[1], parsed[2], parsed[3]]
     }
   }
 
-  relativeLuminance(rgb) {
-    var rgbScaled = rgb.map(x => x/255);
-    var rgbAdjusted = this.adjustRgb(rgbScaled);
-    var luminance = rgbAdjusted[0]*0.2126 + rgbAdjusted[1]*0.7152 + rgbAdjusted[2]*0.0722;
+  relativeLuminance (rgb) {
+    var rgbScaled = rgb.map(x => x / 255)
+    var rgbAdjusted = this.adjustRgb(rgbScaled)
+    var luminance = rgbAdjusted[0] * 0.2126 + rgbAdjusted[1] * 0.7152 + rgbAdjusted[2] * 0.0722
 
-    return luminance;
+    return luminance
   }
 
-  contrast(luminance) {
-    var ratio = (luminance + 0.05) / (0.05);
+  contrast (luminance) {
+    var ratio = (luminance + 0.05) / (0.05)
 
-    return ratio;
+    return ratio
   }
 
-  adjustRgb(rgb) {
-    var r = rgb[0] <= 0.03928 ? rgb[0]/12.92 : Math.pow(((rgb[0]+0.055)/1.055), 2.4);
-    var g = rgb[1] <= 0.03928 ? rgb[1]/12.92 : Math.pow(((rgb[1]+0.055)/1.055), 2.4);
-    var b = rgb[2] <= 0.03928 ? rgb[2]/12.92 : Math.pow(((rgb[2]+0.055)/1.055), 2.4);
-    var adjusted = [r, g, b];
-    
-    return adjusted;
+  adjustRgb (rgb) {
+    var r = rgb[0] <= 0.03928 ? rgb[0] / 12.92 : Math.pow(((rgb[0] + 0.055) / 1.055), 2.4)
+    var g = rgb[1] <= 0.03928 ? rgb[1] / 12.92 : Math.pow(((rgb[1] + 0.055) / 1.055), 2.4)
+    var b = rgb[2] <= 0.03928 ? rgb[2] / 12.92 : Math.pow(((rgb[2] + 0.055) / 1.055), 2.4)
+    var adjusted = [r, g, b]
+
+    return adjusted
   }
 
-  brighten(rgb) {
-    var maximumValue = Math.max(...rgb);
-    var diff = 255 - maximumValue;
-    var rgbBrightened = rgb.map(x => parseInt(x) + diff);
+  brighten (rgb) {
+    var maximumValue = Math.max(...rgb)
+    var diff = 255 - maximumValue
+    var rgbBrightened = rgb.map(x => parseInt(x) + diff)
 
-    return rgbBrightened;
+    return rgbBrightened
   }
 }
-
